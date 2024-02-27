cfc6b05855edbf3e7c4148ec53a991a3100a81fa
khal45 <khalifaariko@gmail.com>
2024-02-27 02:01:07 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// Basic Configuration
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// Your first API endpoint
+app.get('/api/hello', (req, res) => {
+  res.json({ greeting: 'hello API' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
